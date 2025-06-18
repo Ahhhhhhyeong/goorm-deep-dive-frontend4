69d10190810fe7a17f7243ddc11533d71ccc2812
Ahhhhhyeong <dkdud081@gmail.com>
2025-06-18 09:57:22 +0900

    오전 수업내용 업로드

---
+++ src/Day28/day28-react/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import TailwindCssEx from './components/TailwindCssEx';
+
+function App() {
+  return (
+    <>
+      <TailwindCssEx />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Day28/day28-react/src/components/TailwindCssEx.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function TailwindCssEx() {
+  return (
+    <div className='bg-sky-400 text-white p-4 m-4 shadow-md'>
+      <p className='font-mono text-xl'>간단한 tailwind 라이브러리</p>
+    </div>
+  );
+}
